115fd69b3ced24caf7dff1405933f12be10eb70c
wendellfranck <91226109+wendellfranck@users.noreply.github.com>
2025-08-18 16:35:02 -0300

    feat: layout + forrm pt1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'login_page_angular_java_spring';
+  title = 'Login page';
 }
